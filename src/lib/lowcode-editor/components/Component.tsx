91429ec9a6ed59f313d3f90218b0acc946139799
Chez <1242135295@qq.com>
2022-05-11 11:00:01 +0800

    feat(lowcode): 增加属性编辑器

---
+++ src/lib/lowcode-editor/components/Component.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+type Props = {
+    id: string | React.Key,
+    name: string,
+    children: React.Component[]
+}
+
+export const Component = () => {
+    
+}
---
+++ src/lib/lowcode-editor/components/ProjectRenderer.tsx
@@ -0,0 +1,12 @@
+
+type Props = {
+    project: Project
+}
+
+const map = {
+
+}
+
+export const ProjectRenderer = ({ project }: Props) => {
+    // const tree = project.components.map()
+}
