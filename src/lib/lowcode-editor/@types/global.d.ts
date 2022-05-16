46658b462934eb23e35263db23113caf91b8d0fe
cos <3450414733@qq.com>
2022-05-17 03:49:17 +0800

    feat(@types): component属性进行细分，增加实例属性定义instance

--- src/lib/lowcode-editor/@types/global.d.ts
+++ src/lib/lowcode-editor/@types/global.d.ts
@@ -1,9 +1,20 @@
 /**
  * 属性值 支持的类型
  */
-type ValueType = 'string' | 'number' | 'upload' | 'select' | 'switch' | 'color' | 'time' | 'range'
+type ValueType =
+    | 'string'
+    | 'number'
+    | 'upload'
+    | 'select'
+    | 'switch'
+    | 'color'
+    | 'time'
+    | 'range'
+    | 'value'
+    | 'src'
+    | 'body';
 
 /**
  * 组件类型
  */
-type ComponentType = 'Text' | 'Video' | 'Image' | 'Audio' | 'HouseCard' | 'Layout'
+type ComponentType = 'Text' | 'Video' | 'Image' | 'Audio' | 'HouseCard' | 'Layout';
